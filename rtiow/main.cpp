70e24298a827829730c9e0de7ea880978eafdffb
OnurOzdemir0 <onur.ozdemir@ozu.edu.tr>
2024-06-01 19:08:30 +0300

    bozdum

--- rtiow/main.cpp
+++ rtiow/main.cpp
@@ -1,30 +1,40 @@
-#include "main.h"
+#include <iostream>
+#include "vec3.h"
 #include "ray.h"
 #include "color.h"
-#include <iostream>
 
 color ray_color(const ray& r) {
-	return color(0, 0, 0);
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 int main() {
-	auto aspect_ratio = 16.0 / 9.0; //16 width, 9 height
-	int image_width = 400; //400 width ?? height
-	int image_height = int(image_width / aspect_ratio);
-	image_height = (image_height < 1) ? 1 : image_height;
-	
-	auto focal_length = 1.0;
-	auto viewport_height = 2.0;
-	auto viewport_width = viewport_height * (double(image_width) / image_height);
-	auto camera_center = point3(0, 0, 0);
+    // Image
+    const int image_width = 256;
+    const int image_height = 256;
 
-	auto viewport_u = vec3(viewport_width, 0, 0);
-	auto viewport_v = vec3(0, -viewport_height, 0);
+    // Camera
+    auto aspect_ratio = double(image_width) / image_height;
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
 
-	auto pixel_delta_u = viewport_u / image_width;
-	auto pixel_delta_v = viewport_v / image_height;
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0.0, 0.0);
+    auto vertical = vec3(0.0, viewport_height, 0.0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
-	auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-	auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-	
-}
+    // Render
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    for (int j = image_height - 1; j >= 0; --j) {
+        for (int i = 0; i < image_width; ++i) {
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    return 0
--- rtiow/ray.h
+++ rtiow/ray.h
@@ -1,24 +1,17 @@
-#ifndef RAY_H
-#define RAY_H
-
+#pragma once
 #include "vec3.h"
 
 class ray {
 public:
-    ray() {}
+    ray();
+    ray(const point3& origin, const vec3& direction);
 
-    ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+    const point3& origin() const;
+    const vec3& direction() const;
 
-    const point3& origin() const { return orig; }
-    const vec3& direction() const { return dir; }
-
-    point3 at(double t) const {
-        return orig + t * dir;
-    }
+    point3 at(double t) const;
 
 private:
     point3 orig;
     vec3 dir;
 };
-
-#endif
