492055b77714674fbc2d9811b72a04a921199b04
OnurOzdemir0 <onur.ozdemir@ozu.edu.tr>
2024-06-02 18:43:32 +0300

    camera move

--- rtiow/camera.h
+++ rtiow/camera.h
@@ -17,6 +17,12 @@ public:
     int image_width = 400;   
     int samples_per_pixel = 100; //antialising
 
+    // ----------------- camera move
+    vec3 position = point3(0, 0, 0);
+    vec3 lookat = point3(0, 0, -1);
+    vec3 up = vec3(0, 1, 0);
+    double fov = 90.0;
+
     void render(const Hittable& world, const std::string& file_name) {
             
         auto file_ppm = file_name + ".ppm";
@@ -67,6 +73,17 @@ public:
         openBMPFile(file_bmp);
     }
 
+    // ----------------- camera move
+    void move(const vec3& direction) {
+        position += direction;
+        initialize();
+    }
+
+    void set_fov(double new_fov) {
+        fov = new_fov;
+        initialize();
+    }
+
 private:
     int image_height;
     point3 origin;      
@@ -78,14 +95,19 @@ private:
         image_height = static_cast<int>(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
 
-        auto viewport_height = 2.0;
+        auto theta = degrees_to_radians(fov);
+        auto h = tan(theta / 2);
+        auto viewport_height = 2.0 * h;
         auto viewport_width = aspect_ratio * viewport_height;
-        auto focal_length = 1.0;
 
-        origin = point3(0, 0, 0);
-        horizontal = vec3(viewport_width, 0.0, 0.0);
-        vertical = vec3(0.0, viewport_height, 0.0);
-        lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+        auto w = unit_vector(position - lookat);
+        auto u = unit_vector(cross(up, w));
+        auto v = cross(w, u);
+
+        origin = position;
+        horizontal = viewport_width * u;
+        vertical = viewport_height * v;
+        lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
     }
 
     ray get_ray(double u, double v) const {
@@ -108,6 +130,8 @@ private:
             << static_cast<int>(255.999 * pixel_color.y()) << ' '
             << static_cast<int>(255.999 * pixel_color.z()) << '\n';
     }
+    
+
 };
 
 #endif
--- rtiow/main.cpp
+++ rtiow/main.cpp
@@ -9,17 +9,35 @@
 int main() {
     // World
     HittableList world;
-    world.add(make_shared<Sphere>(point3(0, 0, -1), 0.5));
+    //world.add(make_shared<Sphere>(point3(0, 0, -1), 0.5));
     world.add(make_shared<Sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
-    Camera cam;
+/*    Camera cam;
     cam.aspect_ratio = 16.0 / 2.0; 
     cam.image_width = 400; 
     cam.samples_per_pixel = 1;
-    cam.render(world, "sa"); // 2 files will be generated, bmp and ppm. 
+    cam.render(world, "sa");*/ // 2 files will be generated, bmp and ppm. 
     // You can use online PPM viewers
     // Or directly open bmp using windows photo or most ide
+
+    Camera cam;
+    cam.aspect_ratio = 1.0 / 1.0;
+    cam.image_width = 300;
+    cam.samples_per_pixel = 100;
+
+    world.add(make_shared<Sphere>(point3(3, 0, -1), 0.7));
+    world.add(make_shared<Sphere>(point3(-3, 0, -3), 0.7));
+    world.add(make_shared<Sphere>(point3(0, 0, -1), 0.7));
+    //cam.render(world, "image_initial");
+
+    //cam.move(vec3(-2, 0, 0));
+    cam.render(world, "image_moved");
+
+    cam.lookat = point3(0, 0, -10);
+    cam.render(world, "image_moved");
+
+
 
     return 0;
 }
