38ae32d21097340ac15af2a8b80568a0fa01a5ee
Can-CS <93473054+Can-CS@users.noreply.github.com>
2024-06-02 19:19:16 +0300

    Refraction and Glass Material

--- rtiow/main.cpp
+++ rtiow/main.cpp
@@ -15,21 +15,28 @@ int main() {
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3); //add another number at end for fuzziness ((x,y,z), fuzz_mutlip)
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.6); //add another number at end for fuzziness ((x,y,z), fuzz_mutlip)
+    // auto material_left = make_shared<dielectric>(1.00 / 1.33); // use only first value for always refraction
+    // hollow glass sphere
+    auto material_left = make_shared<dielectric>(1.50);
+    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
+    //
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.3); //add another number at end for fuzziness ((x,y,z), fuzz_mutlip)
 
     world.add(make_shared<Sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<Sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
     world.add(make_shared<Sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+
+    world.add(make_shared<Sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_bubble)); // hollow glass sphere
+
     world.add(make_shared<Sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     // Camera
     Camera cam;
     cam.aspect_ratio = 16.0 / 9.0; 
     cam.image_width = 400; 
-    cam.samples_per_pixel = 100;
+    cam.samples_per_pixel = 500;
     cam.max_depth = 50;
-    cam.render(world, "2_metal_shiny.ppm");
+    cam.render(world, "hollow_glass_sphere.ppm");
 
     return 0;
 }
--- rtiow/material.h
+++ rtiow/material.h
@@ -58,4 +58,44 @@ private:
     double fuzz;
 };
 
+class dielectric : public material { //mainly for glass and mirror
+public:
+    dielectric(double refraction_index) : refraction_index(refraction_index) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+        const override {
+        attenuation = color(1.0, 1.0, 1.0);
+        double ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+       /* vec3 refracted = refract(unit_direction, rec.normal, ri);
+
+        scattered = ray(rec.p, refracted); */ // for always refract
+
+        double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+        bool cannot_refract = ri * sin_theta > 1.0;
+        vec3 direction;
+
+        if (cannot_refract || reflectance(cos_theta, ri) > random_double()) // only put "cannot_refract" for air bubble
+            direction = reflect(unit_direction, rec.normal);
+        else
+            direction = refract(unit_direction, rec.normal, ri);
+
+        scattered = ray(rec.p, direction); // change this refracted for always refraction
+        return true;
+    }
+
+private:
+    //index of air, vacuum or ratio of materials refractive index / refractive index of enclose material
+    double refraction_index;
+
+    static double reflectance(double cosine, double refraction_index) { //schlick's approx for reflect
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * pow((1 - cosine), 5);
+    }
+};
+
 #endif
