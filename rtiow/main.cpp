780b29fdf4731124cfb09f82d4585976095e7c61
OnurOzdemir0 <onur.ozdemir@ozu.edu.tr>
2024-06-02 21:58:44 +0300

    depth comparison

--- rtiow/main.cpp
+++ rtiow/main.cpp
@@ -11,7 +11,7 @@ int main() {
     HittableList world;
     auto m_yellow = make_shared<lambertian>(color(0.6, 0.6, 0.0));
     auto m_red = make_shared<lambertian>(color(0.7, 0.3, 0.1));
-    auto m_dielectric = make_shared<dielectric>(0.4);
+    auto m_dielectric = make_shared<dielectric>(0.8);
     auto m_metal = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     world.add(make_shared<Sphere>(point3(0, -100.5, -1), 100, m_yellow));
@@ -22,24 +22,8 @@ int main() {
     // Camera
     Camera cam;
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 800;
-    cam.samples_per_pixel = 30;
-    cam.max_depth = 10;
-    cam.render(world, "initial_image_fhd1");
-    m_dielectric = make_shared<dielectric>(0.5);
-    cam.render(world, "initial_image_fhd2");
-    m_dielectric = make_shared<dielectric>(0.6);
-    cam.render(world, "initial_image_fhd3");
-    m_dielectric = make_shared<dielectric>(0.7);
-    cam.render(world, "initial_image_fhd4");
-    m_dielectric = make_shared<dielectric>(0.8);
-    cam.render(world, "initial_image_fhd8");
-    m_dielectric = make_shared<dielectric>(1.1);
-    cam.render(world, "initial_image_fhd5");
-    m_dielectric = make_shared<dielectric>(1.2);
-    cam.render(world, "initial_image_fhd6");
-    m_dielectric = make_shared<dielectric>(2);
-    cam.render(world, "initial_image_fhd7");
-    m_dielectric = make_shared<dielectric>(2.1);
-    return 0;
+    cam.image_width = 1080;
+    cam.samples_per_pixel = 1;
+    cam.max_depth = 20;
+    cam.render(world, "depth6");
 }
